97628a81949852eaf5d616ebefbe817c4a2462cd
whoispim <pimbot@bk.ru>
2022-12-27 00:49:08 +0100

    day 19

--- 19/19.py
+++ 19/19.py
@@ -1,130 +1,85 @@
 import re
 import time
 from math import ceil, prod
+import functools
 
 
-def apply_blueprint_dumb(costs, time_left,
-                         ore=0, clay=0, obs=0,
-                         ore_bot=1, clay_bot=0, obs_bot=0):
-    if time_left <= 0:
-        return 0
-    if time_left < 6 and obs_bot == 0:
-        return 0
-    # print(time_left, ore, clay, obs, ore_bot, clay_bot, obs_bot)
-    new_ore = ore + ore_bot
-    new_clay = clay + clay_bot
-    new_obs = obs + obs_bot
-    time_left -= 1
-    most_geodes = 0
-    # no building
-    profit = apply_blueprint_dumb(costs, time_left,
-                                  new_ore, new_clay, new_obs,
-                                  ore_bot, clay_bot, obs_bot)
-    if profit > most_geodes:
-        most_geodes = profit
-    # build ore_bot
-    if ore >= costs[0]["ore"]:
-        profit = apply_blueprint_dumb(costs, time_left,
-                                      new_ore - costs[0]["ore"],
-                                      new_clay, new_obs,
-                                      ore_bot + 1, clay_bot, obs_bot)
-        if profit > most_geodes:
-            most_geodes = profit
-    # build clay_bot
-    if ore >= costs[1]["ore"]:
-        profit = apply_blueprint_dumb(costs, time_left,
-                                      new_ore - costs[1]["ore"],
-                                      new_clay, new_obs,
-                                      ore_bot, clay_bot + 1, obs_bot)
-        if profit > most_geodes:
-            most_geodes = profit
-    # build obs_bot
-    if ore >= costs[2]["ore"] and clay >= costs[2]["clay"]:
-        profit = apply_blueprint_dumb(costs, time_left,
-                                      new_ore - costs[2]["ore"],
-                                      new_clay - costs[2]["clay"],
-                                      new_obs,
-                                      ore_bot, clay_bot, obs_bot + 1)
-        if profit > most_geodes:
-            most_geodes = profit
-    # build geode_bot
-    if ore >= costs[3]["ore"] and obs >= costs[3]["obsidian"]:
-        profit = (
-                time_left
-                + apply_blueprint_dumb(costs, time_left,
-                                       new_ore - costs[3]["ore"],
-                                       new_clay,
-                                       new_obs - costs[3]["obsidian"],
-                                       ore_bot, clay_bot, obs_bot)
-        )
-        if profit > most_geodes:
-            most_geodes = profit
-
-    return most_geodes
-
-
+@functools.cache
 def apply_blueprint(costs, time_left,
                     ore=0, clay=0, obs=0,
                     ore_bot=1, clay_bot=0, obs_bot=0):
     if time_left <= 0:
         return 0
-    # print(f"T: {time_left}, OCO {ore, clay, obs}, Bots {ore_bot, clay_bot, obs_bot}")
     most_geodes = 0
     # next ore-bot
-    if costs[0]["ore"] <= ore:
-        time_to = 1
-    else:
-        time_to = ceil((costs[0]["ore"] - ore) / ore_bot) + 1
-    if 0 < time_to < time_left:
-        profit = apply_blueprint(costs, time_left - time_to,
-                                 ore + time_to * ore_bot - costs[0]["ore"],
-                                 clay + time_to * clay_bot,
-                                 obs + time_to * obs_bot,
-                                 ore_bot + 1, clay_bot, obs_bot)
-        if profit > most_geodes:
-            most_geodes = profit
-    # next clay-bot
-    time_to = ceil((costs[1]["ore"] - ore) / ore_bot) + 1
-    if 0 < time_to < time_left:
-        profit = apply_blueprint(costs, time_left - time_to,
-                                 ore + time_to * ore_bot - costs[1]["ore"],
-                                 clay + time_to * clay_bot,
-                                 obs + time_to * obs_bot,
-                                 ore_bot, clay_bot + 1, obs_bot)
-        if profit > most_geodes:
-            most_geodes = profit
-    # next obs-bot
-    if clay_bot > 0:
-        time_to = max(
-            ceil((costs[2]["ore"] - ore) / ore_bot),
-            ceil((costs[2]["clay"] - clay) / clay_bot)
-        ) + 1
+    # check if max feasible amount hasn't been reached yet
+    if ore_bot < costs[4]:
+        if costs[0] <= ore:
+            time_to = 1
+        else:
+            time_to = ceil((costs[0] - ore) / ore_bot) + 1
         if 0 < time_to < time_left:
             profit = apply_blueprint(costs, time_left - time_to,
-                                     ore + time_to * ore_bot - costs[2]["ore"],
-                                     clay + time_to * clay_bot - costs[2]["clay"],
+                                     ore + time_to * ore_bot - costs[0],
+                                     clay + time_to * clay_bot,
+                                     obs + time_to * obs_bot,
+                                     ore_bot + 1, clay_bot, obs_bot)
+            if profit > most_geodes:
+                most_geodes = profit
+    # next clay-bot
+    # check if max feasible amount hasn't been reached yet
+    if clay_bot < costs[2][1]:
+        if costs[1] <= ore:
+            time_to = 1
+        else:
+            time_to = ceil((costs[1] - ore) / ore_bot) + 1
+        if 0 < time_to < time_left:
+            profit = apply_blueprint(costs, time_left - time_to,
+                                     ore + time_to * ore_bot - costs[1],
+                                     clay + time_to * clay_bot,
+                                     obs + time_to * obs_bot,
+                                     ore_bot, clay_bot + 1, obs_bot)
+            if profit > most_geodes:
+                most_geodes = profit
+    # next obs-bot
+    # check if base resource can be produced and if max feasible
+    # amount hasn't been reached yet
+    if clay_bot > 0 and obs_bot < costs[3][1]:
+        if costs[2][0] <= ore and costs[2][1] <= clay:
+            time_to = 1
+        else:
+            time_to = max(
+                ceil((costs[2][0] - ore) / ore_bot),
+                ceil((costs[2][1] - clay) / clay_bot)
+            ) + 1
+        if 0 < time_to < time_left:
+            profit = apply_blueprint(costs, time_left - time_to,
+                                     ore + time_to * ore_bot - costs[2][0],
+                                     clay + time_to * clay_bot - costs[2][1],
                                      obs + time_to * obs_bot,
                                      ore_bot, clay_bot, obs_bot + 1)
             if profit > most_geodes:
                 most_geodes = profit
     # next geode-bot
     if obs_bot > 0:
-        time_to = max(
-            ceil((costs[3]["ore"] - ore) / ore_bot),
-            ceil((costs[3]["obsidian"] - obs) / obs_bot)
-        ) + 1
+        if costs[3][0] <= ore and costs[3][1] <= obs:
+            time_to = 1
+        else:
+            time_to = max(
+                ceil((costs[3][0] - ore) / ore_bot),
+                ceil((costs[3][1] - obs) / obs_bot)
+            ) + 1
         if 0 < time_to < time_left:
             profit = (apply_blueprint(costs, time_left - time_to,
-                                      ore + time_to * ore_bot - costs[3]["ore"],
+                                      ore + time_to * ore_bot - costs[3][0],
                                       clay + time_to * clay_bot,
-                                      obs + time_to * obs_bot - costs[3]["obsidian"],
+                                      obs + time_to * obs_bot - costs[3][1],
                                       ore_bot, clay_bot, obs_bot)
                       + time_left - time_to)
-            # print(f"left: {time_left}, next: {time_to}, geodes: {time_left - time_to}"
-            #       f"   ore: {ore}, obs: {obs}")
             if profit > most_geodes:
                 most_geodes = profit
     return most_geodes
+
 
 blueprints = []
 with open("input", "r") as f:
@@ -132,15 +87,21 @@ with open("input", "r") as f:
         rob_list = []
         for robot in line.split(":")[1].split(".")[:-1]:
             match = re.findall("([0-9]+) ([a-z]+)", robot)
-            rob_dict = {}
+            rob_tup = []
             for group in match:
-                rob_dict[group[1]] = int(group[0])
-            rob_list.append(rob_dict)
-        blueprints.append(rob_list)
+                rob_tup.append(int(group[0]))
+            if len(rob_tup) == 1:
+                rob_list.append(rob_tup[0])
+            else:
+                rob_list.append(tuple(rob_tup))
+        # add max clay need to end
+        rob_list.append(max(rob_list[0], rob_list[1], rob_list[2][0]))
+        blueprints.append(tuple(rob_list))
 
 start_time = time.time()
 quality_levels = []
 for i, blue in enumerate(blueprints):
+    apply_blueprint.cache_clear()
     quality_levels.append(
         apply_blueprint(blue, 24) * (i+1)
     )
@@ -152,6 +113,7 @@ print(f"{time.time() - start_time}s")
 start_time = time.time()
 geodes_opened = []
 for i, blue in enumerate(blueprints[:3]):
+    apply_blueprint.cache_clear()
     geodes_opened.append(
         apply_blueprint(blue, 32)
     )
@@ -159,6 +121,4 @@ for i, blue in enumerate(blueprints[:3]):
 print("--- Second part: ---")
 print(f"Geodes opened: {geodes_opened}")
 print(f"Product of opened Geodes: {prod(geodes_opened)}")
-# 79560 too high [26, 102, 30]
-#                 26,  51, 10
-# 13260 too low
+
